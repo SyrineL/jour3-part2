5fe8609cb88a08e4d9a9d149358dc351c142f4e0
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-09-29 14:47:55 +0200

    first commit

---
+++ jour3part2.js
@@ -0,0 +1,24 @@
+
+//
+  var nbre = 0;
+  for(i=12; i<48; i=i+2)
+  {
+    nbre = i;
+    console.log(nbre)
+  }
+  
+//
+
+  var nbre = 0;
+  for(i=109; i<588; i=i+2)
+  {
+    nbre = i;
+    console.log(nbre)
+  }
+  
+  //
+
+var numbers = [1, 2, 3, 4, 5]
+var squares = []
+var roots = []
+
